2908e0b9e209fb0a33a89a2f7b8833f27ff7e322
KhanhNCK <khanhcong696@gmail.com>
2019-08-09 12:12:33 +0700

    add part 2

--- js/trangchu.js
+++ js/trangchu.js
@@ -105,4 +105,34 @@ $backToTop.on('click', function(e) {
   $("html, body").animate({scrollTop: 0}, 500);
 });
 
+//chitietdichvu
 
+
+var coll = document.getElementsByClassName("collapsible");
+var i;
+
+for (i = 0; i < coll.length; i++) {
+  coll[i].addEventListener("click", function() {
+    this.classList.toggle("active");
+    var content = this.nextElementSibling;
+    if (content.style.display === "block") {
+      content.style.display = "none";
+    } else {
+      content.style.display = "block";
+    }
+  });
+}
+
+
+window.onscroll = function() {myFunction()};
+
+    var navbar = document.getElementById("bg-light");
+    var sticky = navbar.offsetTop;
+
+    function myFunction() {
+      if (window.pageYOffset >= sticky) {
+        navbar.classList.add("sticky")
+      } else {
+        navbar.classList.remove("sticky");
+      }
+    }
